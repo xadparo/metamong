99885fbc988c49f83907363a12b6f80122e63756
xadparo <45482148+xadparo@users.noreply.github.com>
2021-11-15 17:16:24 +0900

    build: watch, dev 스크립트 병합 & 개발 서버 자동 실행

--- build.js
+++ build.js
@@ -1,16 +1,14 @@
 const esbuild = require('esbuild')
 const process = require('process')
 const moment = require('moment')
+const childprocess = require('child_process')
+const chalk = require('chalk')
 
 const argv = process.argv.slice(2)
 const argm = {}
 argv.forEach((arg) => argm[arg] = true)
 
-const watch = (argm['-w'] || false) && {
-  onRebuild() {
-    console.log(`Rebuilded, ${moment().format('HH:mm:ss')}`)
-  },
-}
+const watch = argm['-w'] || false
 
 const originEnv = { ...process.env }
 require('dotenv').config()
@@ -23,18 +21,39 @@ for (const key in process.env) {
   define[`process.env.${key}`] = JSON.stringify(process.env[key] || '')
 }
 
+const processManager = {
+  child: null,
+  endChild() {
+    this.child?.kill()
+    this.child = null
+  },
+  startChild() {
+    this.endChild()
+
+    const child = this.child = childprocess.spawn('node', ['dist/main'])
+    child.stdout.pipe(process.stdout)
+    console.log(chalk.blueBright`[Builder] Server (re)start ${new Date}`)
+  },
+}
+
+if (watch) {
+  processManager.startChild()
+}
 esbuild.build({
   entryPoints: ['src/main.ts'],
   platform: 'node',
 
   define,
 
-  watch,
+  watch: watch && {
+    onRebuild() {
+      console.log(chalk.blueBright`[Builder] BE Rebuild ${new Date}`)
+      processManager.startChild()
+    },
+  },
   bundle: true,
   minify: !watch,
   sourcemap: watch? 'both': false,
   outdir: 'dist',
   tsconfig: 'tsconfig.json',
 })
-  .then(() => console.log('Build success.'))
-  .catch((err) => console.error('Build error:', err.message))
